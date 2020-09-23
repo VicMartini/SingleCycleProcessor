ac9f008fbc7604c7f93660ae8372f6b8e1d45d97
Victor Martini <vmartini@localhost.localdomain>
2020-09-23 13:19:54 -0300

    Aded tests to the alus's testbench, Tests OK

--- alu_tb.sv
+++ alu_tb.sv
@@ -11,13 +11,59 @@ module alu_tb();
 	initial
 		begin
 			errors = 0;
-			for ( i = 0; i < 10; i +=1) 
+			for ( i = 0; i < 10; i +=1) // Both positive
 				begin
 					for ( j = 0; j < 10; j +=1)
 						begin
-							ALUControl = 4'b0010; a = 64'(i); b = 64'(j);#5;
+
+							ALUControl = 4'b0000; a = 64'(i); b = 64'(j);#5; //AND
+							if (result !== (64'(i) & 64'(j))) errors += 1;
+
+							ALUControl = 4'b0001; a = 64'(i); b = 64'(j);#5; //OR
+							if (result !== (64'(i) | 64'(j))) errors += 1;							
+							
+							ALUControl = 4'b0010; a = 64'(i); b = 64'(j);#5; // ADD
 							if (result !== 64'(i) + 64'(j)) errors += 1;
-							$display("result %d", result);
+
+							ALUControl = 4'b0110; a = 64'(i); b = 64'(j);#5; // SUB
+							if (result !== 64'(i) - 64'(j)) errors += 1;
+
+							ALUControl = 4'b0111; a = 64'(i); b = 64'(j);#5; // PASS B
+							if (result !== 64'(j)) errors += 1;
+							
+							ALUControl = 4'b1100; a = 64'(i); b = 64'(j);#5; // NOR 
+							if (result !== ~(64'(i) | 64'(j)))errors += 1; 						
+
+							
+														
+						end
+				end
+				
+				for ( i = 0; i < 10; i +=1) // One negative and one positive
+				begin
+					for ( j = 0; j > -10; j -=1)
+						begin
+
+							ALUControl = 4'b0000; a = 64'(i); b = 64'(j);#5; //AND
+							if (result !== (64'(i) & 64'(j))) errors += 1;
+
+							ALUControl = 4'b0001; a = 64'(i); b = 64'(j);#5; //OR
+							if (result !== (64'(i) | 64'(j))) errors += 1;							
+							
+							ALUControl = 4'b0010; a = 64'(i); b = 64'(j);#5; // ADD
+							if (result !== 64'(i) + 64'(j)) errors += 1;
+
+							ALUControl = 4'b0110; a = 64'(i); b = 64'(j);#5; // SUB
+							if (result !== 64'(i) - 64'(j)) errors += 1;
+
+							ALUControl = 4'b0111; a = 64'(i); b = 64'(j);#5; // PASS B
+							if (result !== 64'(j)) errors += 1;
+							
+							ALUControl = 4'b1100; a = 64'(i); b = 64'(j);#5; // NOR 
+							if (result !== ~(64'(i) | 64'(j)))errors += 1; 						
+
+							
+														
 						end
 				end
 				$display("The simulation has finished, %d errors occurred", errors);
