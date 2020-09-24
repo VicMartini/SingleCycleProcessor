18f639be17b9353d71283ae914f0f5ee45b15078
Victor Martini <vmartini@localhost.localdomain>
2020-09-24 15:25:38 -0300

    Fixed RegWrite activation problem

--- maindec.sv
+++ maindec.sv
@@ -6,10 +6,10 @@ module maindec(input logic [10:0] Op,
 		begin
 			$display("%b",Op);
 			casez(Op)
-				11'b11111000010 : {Reg2Loc, ALUSrc, MemtoReg, MemRead, RegWrite, MemWrite, Branch, ALUOp[1:0]}= 9'b000100010; // LDUR
-				11'b11111000000 : {Reg2Loc, ALUSrc, MemtoReg, MemRead, RegWrite, MemWrite, Branch, ALUOp[1:0]}= 9'b110001000; // STUR
-				11'b10110100??? : {Reg2Loc, ALUSrc, MemtoReg, MemRead, RegWrite, MemWrite, Branch, ALUOp[1:0]} = 9'b100000101; // CBZ
-				11'b???0101?000 : {Reg2Loc, ALUSrc, MemtoReg, MemRead, RegWrite, MemWrite, Branch, ALUOp[1:0]}= 9'b000100010; // R Instructions			
+				11'b11111000010 : {Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0]}=  9'b000100010; // LDUR
+				11'b11111000000 : {Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0]}=  9'b110001000; // STUR
+				11'b10110100??? : {Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead,  MemWrite, Branch, ALUOp[1:0]} = 9'b100000101; // CBZ
+				11'b???0101?000 : {Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0]}=  9'b000100010; // R Instructions			
 				default : {Reg2Loc, ALUSrc, MemtoReg, MemRead, RegWrite, MemWrite, Branch, ALUOp[1:0]} = '0;
 			endcase
 		end
