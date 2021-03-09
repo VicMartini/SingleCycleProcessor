45712ea92b9d71908f7182d478e1fb6913181c89
Victor Martini <victor.cesar.m@gmail.com>
2021-03-08 21:12:28 -0300

    Fixed wrong value for the output after a reset signal.

--- esync.sv
+++ esync.sv
@@ -2,8 +2,8 @@ module ESync(input logic set, reset, output logic OUT);
 
 	always_ff @(posedge set, posedge reset)
 		begin
-			if (set) OUT = 1;
-			if (reset) OUT = 1;
+			if (set) OUT = 1'b1;
+			if (reset) OUT = 1'b0;
 		end
 		
 endmodule
